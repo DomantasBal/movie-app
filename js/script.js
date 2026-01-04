fc227ceb709a1c2f16e71a0296a345bd3a91e41a
Domantas <92823413+DomantasBal@users.noreply.github.com>
2026-01-04 11:40:25 +0200

    FEAT: popular movies
    Added movies render

--- js/script.js
+++ js/script.js
@@ -2,6 +2,45 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+  console.log(results);
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('.card');
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />`
+                : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+  `;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+async function fetchAPIData(endpoint) {
+  // THIS IS FREE API. Anyone can get it and it is for learning purposes.
+  // Storing api key client side is BAD I know :)
+  const API_KEY = 'a9cad226792844ffde25e3688c7cc1f7'; // BAD PRACTICE - just for learning purposes
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+  const data = await response.json();
+  return data;
+}
+
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
   links.forEach((link) => {
@@ -15,6 +54,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displayPopularMovies();
       break;
     case '/shows.html':
       break;
