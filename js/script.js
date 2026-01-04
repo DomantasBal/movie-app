a90a82f621e4816a974a740733e4504246ecc533
Domantas <92823413+DomantasBal@users.noreply.github.com>
2026-01-04 13:32:04 +0200

    FEAT: single movie page
    filled single movie page

--- js/script.js
+++ js/script.js
@@ -30,15 +30,107 @@ async function displayPopularMovies() {
   });
 }
 
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('.card');
+
+    console.log(show);
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `
+                <img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />`
+                : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${show.first_air_date}</small>
+            </p>
+          </div>
+  `;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+   <div class="details-top">
+          <div>
+    ${
+      movie.poster_path
+        ? `
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />`
+        : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
+    }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+            ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          ${movie.production_companies.map((company) => `<span>${company.name}</span> | `).join('')}
+        </div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 async function fetchAPIData(endpoint) {
   // THIS IS FREE API. Anyone can get it and it is for learning purposes.
   // Storing api key client side is BAD I know :)
   const API_KEY = 'a9cad226792844ffde25e3688c7cc1f7'; // BAD PRACTICE - just for learning purposes
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
   const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
   const data = await response.json();
+  hideSpinner();
+
   return data;
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
 }
 
 function highlightActiveLink() {
@@ -57,8 +149,10 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
+      displayPopularShows();
       break;
     case '/movie-details.html':
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       break;
